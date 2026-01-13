9ada2ffa0763a44662c331af70ef269c9a259adc
djalabz <djalabz@gmail.com>
2026-01-13 14:15:40 +0100

    :sparkles: page de signup créée

---
+++ signup.php
@@ -0,0 +1,20 @@
+<?php
+
+include "partials/header.php";
+
+?>
+
+<h1>Page de Signup</h1>
+
+<!-- 
+1 - Coder le form de signup : username, email, mot de passe et confirmation du mot de passe
+2 - Il faut transmettre les données via POST
+3 - Créer une table users dans phpmyadmin qui contiendra les infos de chaque user
+3 - Si le username, le mail et le mdp sont bien remplis on les enregistre en BDD 
+-->
+
+<?php
+
+include "partials/footer.php";
+
+?>
