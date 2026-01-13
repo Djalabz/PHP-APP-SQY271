6e2365e7be9c5991961e35a42493a5902d5eb75f
djalabz <djalabz@gmail.com>
2026-01-13 11:09:48 +0100

    :sparkles: correction pour le formulaire en POST ajoutée

--- sondage.php
+++ sondage.php
@@ -5,9 +5,32 @@ include "partials/header.php";
 // Coder le php ici ...
 
 // 1 - Vérifier que le formulaire ait bien été soumis 
-// 2 - Vérifier que tous les champs/ inputs aient bien été pris en compte
-// 3 - Une fois tout cela vérifié on recupèe les différentes valeurs que l'on assigne à des variables 
-// 4 - Enfin on les affiche dans le HTML en y mélangeant les balises php Si pb lors des vérifications 
+// On va utiliser la superglobale $_POST pour recup les infos transmises via la méthode http POST
+if (isset($_POST["submit"])) {
+
+    // 2 - Vérifier que tous les champs/ inputs aient bien été pris en compte
+    if (!empty($_POST["prenom"]) 
+    && !empty($_POST["nom"]) 
+    && !empty($_POST["language"]) 
+    && !empty($_POST["gafa"]) 
+    && !empty($_POST["front"]) 
+    && !empty($_POST["ia"])) {
+
+        // 3 - Une fois tout cela vérifié on recupère les différentes valeurs que l'on assignent à des variables 
+        $prenom = $_POST["prenom"];
+        $nom = $_POST["nom"];
+        $language = $_POST["language"];
+        $gafa = $_POST["gafa"];
+        $front = $_POST["front"];
+        $ia = $_POST["ia"]; 
+
+    } else {
+        $error = "Veuillez remplir tous les champs";
+    }
+} 
+
+
+// 4 - Enfin on les affiche dans le HTML en y mélangeant les balises php si pb lors des vérifications 
 // afficher un message d'erreur 
 
 // Vous aurez basoin de récupérer les infos soumises par l'utilisateur pour ca on peut utiliser $_POST
@@ -50,17 +73,28 @@ include "partials/header.php";
     <br>
     <br>
 
-    <input value="Soumettre" type="submit">
+    <input name="submit" value="Soumettre" type="submit">
 
 </form>
 
 <div class="results">
 
-    <h3>Vous etes ...</h3>
-    <h3>Votre language favori est ...</h3>
-    <h3>Votre GAFA favori est ...</h3>
-    <h3>Plutot ...</h3>
-    <h3>Peur de l'IA ?</h3>
+    <!-- On vient afficher les variables déclarées plus haut ssi on a pas d'erreur 
+    et que le formulaire a bien été soumis   -->
+    <?php if (!isset($error) && isset($_POST["submit"])) : ?>
+
+        <h3>Vous etes <?= $prenom ?> <?= $nom ?></h3>
+        <h3>Votre language favori est <?= $language ?></h3>
+        <h3>Votre GAFA favori est <?= $gafa ?></h3>
+        <h3>Plutot <?= $front ?></h3>
+        <h3>Peur de l'IA ? <?= $ia ?></h3>
+
+    <!-- Cas ou on a une erreur et le formulaire a été soumis  -->
+    <?php elseif (isset($error) && isset($_POST["submit"])) : ?>
+
+        <h3><?= $error ?></h3>
+
+    <?php endif ?>
 
 </div>
 
