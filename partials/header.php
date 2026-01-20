eaa20db2dadbc2865f8df82df2185f3a3ba8fa02
djalabz <djalabz@gmail.com>
2026-01-20 14:26:55 +0100

    :children_crossing: Empeche user non login d'accéder à certaines pages

--- partials/header.php
+++ partials/header.php
@@ -1,3 +1,9 @@
+<?php 
+
+session_start();
+
+?>
+
 <!DOCTYPE html>
 
 <html lang="en">
@@ -8,11 +14,29 @@
     <link rel="stylesheet" href="style.css" type="text/css">
 </head>
 <body>
+
+<header>
+    <nav>
+        <?php if (isset($_SESSION["username"])) : ?>
+
+            <a href="index.php">Home</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+            <a href="logout.php">Logout</a>
+
+        <?php else : ?>
+
+            <a href="login.php">Login</a>
+            <a href="signup.php">Signup</a>
+
+        <?php endif ?>
+    </nav>
+</header>
     
-1 - Faire un menu de navigation -> Home / About / Contact / Signout
+<!-- 1 - Faire un menu de navigation -> Home / About / Contact / Signout
 
 2 - !! Si jamais la personne ,n'est pas connectée (cad que la session n'est pas créee) 
 alors on affiche juste Signup / Login dans le menu
 
 3 - Rajouter en BDD dans users une colonne avatar qui contiendra le chemin vers un avatar par défaut
-Le user pourra le changer ultérieurement mais en attendant l'avatar par défaut doit s'afficher sur la homes
+Le user pourra le changer ultérieurement mais en attendant l'avatar par défaut doit s'afficher sur la homes -->
