e300b8169f6ea028eedd074e56ada59d9980f125
djalabz <djalabz@gmail.com>
2026-01-22 11:09:17 +0100

    :lipstick: suignup tailwind fonctionnel

--- signup.php
+++ signup.php
@@ -74,7 +74,52 @@ if (isset($_POST["submit"])) {
 4 - Si le username, le mail et le mdp sont bien remplis on les enregistre en BDD 
 -->
 
-<h1>Page de Signup</h1>
+<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+    <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Signup</h2>
+  </div>
+
+  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+
+    <form action="#" method="POST" class="space-y-6">
+      <div>
+        <label for="email" class="block text-sm/6 font-medium text-gray-900">Email</label>
+        <div class="mt-2">
+          <input id="email" type="email" name="email" required autocomplete="email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+        </div>
+        <label for="username" class="block text-sm/6 font-medium text-gray-900">Username</label>
+        <div class="mt-2">
+          <input id="username" type="username" name="username" required autocomplete="username" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+        </div>
+      </div>
+
+      <div>
+        <div class="flex items-center justify-between">
+          <label for="password" class="block text-sm/6 font-medium text-gray-900">Mot de passe</label>
+          <div class="text-sm">
+            <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Mot de passe oublié ?</a>
+          </div>
+        </div>
+        <div class="mt-2">
+          <input id="password" type="password" name="password" required autocomplete="current-password" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+        </div>
+      </div>
+
+      <div>
+        <input value="Signup" type="submit" name="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+      </div>
+    </form>
+
+    <?php if (isset($error)) : ?> 
+
+        <h3 class="mt-10 text-center text-sm/6 text-red-500"><?= $error ?></h3>
+
+    <?php endif ?> 
+
+  </div>
+</div>
+
+<!-- <h1>Page de Signup</h1>
 
 <form action="" method="POST">
 
@@ -91,7 +136,7 @@ if (isset($_POST["submit"])) {
 
     <h3><?= $error ?></h3>
 
-<?php endif ?>
+<?php endif ?> -->
 
 <?php
 
