0ea367d91f65ff2e98b58508ebfea937b2b5174a
djalabz <djalabz@gmail.com>
2026-01-22 15:18:42 +0100

    :sparkles: Panier vraiment fonctionnel

--- add-to-cart.php
+++ add-to-cart.php
@@ -17,12 +17,27 @@
         $stmt->execute([$_SESSION["id"]]);  
         $res = $stmt->fetch();
 
+        // var_dump($res["content"]);
+
         if ($res) {
             // Cas ou il y a déjà un cart d'enregistré 
+            $sqlUpdate = "UPDATE cart SET content = ? WHERE id_user = ?";
 
+            $content = json_decode($res["content"]);
+
+            $content[] = $_SESSION["current-product"];
+
+            $content = json_encode($content);
+
+
+            $stmtUpdate = $db->prepare($sqlUpdate);
+            $stmtUpdate->execute([$content, $_SESSION["id"]]);  
+            $res = $stmtUpdate->fetch();
+
+            header("Location: shop-item.php?id=$productId&status=success");
 
         } else {
-            // Cas ou il faut aussi créer le cart dans la BDD 
+            // Cas ou il faut aussi créer le panier dans la BDD 
             // Requete pour créer un panier dans la table cart
             $sqlCreate = "INSERT INTO cart(content, id_user) VALUES(?, ?)";
 
@@ -32,7 +47,6 @@
             $stmtCreate = $db->prepare($sqlCreate);
             $stmtCreate->execute([$content, $_SESSION["id"]]);  
             $resCreate = $stmtCreate->fetch();
-
 
             header("Location: shop-item.php?id=$productId&status=success");
         }
