aed35a784666634b9bf57d35d6255719c8e278b2
djalabz <djalabz@gmail.com>
2026-01-29 11:37:37 +0100

    :sparkles: Page de contact fonctionnelle !

--- contact.php
+++ contact.php
@@ -3,20 +3,34 @@
 include "partials/header.php";
 include "partials/check-session.php";
 
+if (isset($_GET["error"])) {
+    $error = "Veuillez remplir tous les champs";
+} elseif (isset($_GET["success"])) {
+    $message = "Votre mail a bien été envoyé !";
+}
 
 ?>
 
 <!-- Page de contact  -->
-
 <div class="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
-  <div aria-hidden="true" class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
+
+
+  <!-- <div aria-hidden="true" class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
     <div style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" class="relative left-1/2 -z-10 aspect-1155/678 w-144.5 max-w-none -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-288.75"></div>
-  </div>
+  </div> -->
   <div class="mx-auto max-w-2xl text-center">
     <h2 class="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">Contact</h2>
-    <p class="mt-2 text-lg/8 text-gray-600">Contactez nous par mail via le formulaire ci-dessous</p>
+    <p class="mt-4 text-lg/8 text-gray-600">Contactez nous par mail via le formulaire ci-dessous</p>
   </div>
-  <form action="#" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
+
+    <?php if (isset($message)) : ?>
+
+        <h3 class="text-green-500 text-center mt-4"><?= $message ?></h3>
+
+    <?php endif ?>
+
+    <!-- Formulaire de contact en POST -->
+  <form action="contact-process.php" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
     <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
       <div>
         <label for="first-name" class="block text-sm/6 font-semibold text-gray-900">Prénom</label>
@@ -31,33 +45,9 @@ include "partials/check-session.php";
         </div>
       </div>
       <div class="sm:col-span-2">
-        <label for="company" class="block text-sm/6 font-semibold text-gray-900">Entreprise</label>
-        <div class="mt-2.5">
-          <input id="company" type="text" name="company" autocomplete="organization" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
-        </div>
-      </div>
-      <div class="sm:col-span-2">
         <label for="email" class="block text-sm/6 font-semibold text-gray-900">Email</label>
         <div class="mt-2.5">
           <input id="email" type="email" name="email" autocomplete="email" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
-        </div>
-      </div>
-      <div class="sm:col-span-2">
-        <label for="phone-number" class="block text-sm/6 font-semibold text-gray-900">Téléphone</label>
-        <div class="mt-2.5">
-          <div class="flex rounded-md bg-white outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
-            <div class="grid shrink-0 grid-cols-1 focus-within:relative">
-              <select id="country" name="country" autocomplete="country" aria-label="Country" class="col-start-1 row-start-1 w-full appearance-none rounded-md py-2 pr-7 pl-3.5 text-base text-gray-500 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-                <option>US</option>
-                <option>CA</option>
-                <option>EU</option>
-              </select>
-              <svg viewBox="0 0 16 16" fill="currentColor" data-slot="icon" aria-hidden="true" class="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4">
-                <path d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" fill-rule="evenodd" />
-              </svg>
-            </div>
-            <input id="phone-number" type="text" name="phone-number" placeholder="123-456-7890" class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
-          </div>
         </div>
       </div>
       <div class="sm:col-span-2">
@@ -79,10 +69,16 @@ include "partials/check-session.php";
       </div>
     </div>
     <div class="mt-10">
-      <button type="submit" class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Envoyer le mail</button>
+      <button name="submit" type="submit" class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Envoyer le mail</button>
     </div>
   </form>
 </div>
+
+<?php if (isset($error)) : ?>
+
+    <h3 class="text-red-500"><?= $error ?></h3>
+
+<?php endif ?>
 
 
 <?php
