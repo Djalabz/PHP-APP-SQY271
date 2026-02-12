f009aa06576e98e972d31106e7a891842aa55aef
djalabz <djalabz@gmail.com>
2026-02-12 13:00:54 +0100

    :sparkles: Todo avec local storage fonctionelle

--- scripts/app.js
+++ scripts/app.js
@@ -12,7 +12,6 @@ crossBtn.forEach(btn => {
     })
 })
 
-
 // TODO APP 
 
 // Récupérer contenu de l'input lors du click
@@ -22,70 +21,100 @@ const todoInput = document.querySelector("#todo-input")
 const todoSubmit = document.querySelector(".todo-submit")
 const todosList = document.querySelector(".todos-zone")
 
-todoSubmit.addEventListener("click", () => {
-    if (todoInput.value != "") {
-        // Je créee un élément HTML avec pour balise div
-        let todoDiv = document.createElement("div")
-        todoDiv.classList.add("relative")
-        // Il faudra ajouter de bons styles pour la todo 
-        todoDiv.innerHTML = `<a href="#" class="mt-2 bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs hover:bg-neutral-secondary-medium">
-                <h5 class="todoContent mb-3 text-xl font-semibold tracking-tight text-heading leading-8">` + todoInput.value + `</h5>
-        </a>`
+// Initialisation du tableau de mes todos pour le local storage 
+// Ici comprendre, on récup les todos depuis le LS mais si on ne trouve rien on déclare un tableau vide 
+let todosArray = JSON.parse(localStorage.getItem("todos")) || []
 
-        // Création des boutons (ou plutot mes inputs et img qui serviront de boutons) pour chaque div
-        let checkBtn = document.createElement("div")
-        let editBtn = document.createElement("a")
-        let closeBtn = document.createElement("a")
-
-        // Je donne du contenu à mes boutons 
-        checkBtn.innerHTML = `<input class="w-4" type="checkbox" />`
-        editBtn.innerHTML = `<img class="w-6 " src="./assets/icons/edit.svg" />`
-        closeBtn.innerHTML = `<img class="w-6 " src="./assets/icons/close-cross.svg" >`
-
-        // Logique du bouton check -> on écoute le click et on change les classes pour ajouter la ligne 
-        // et modifier l'opacité 
-        checkBtn.addEventListener("click", () => {
-            todoDiv.classList.toggle("line-through")
-            todoDiv.classList.toggle("opacity-50")
-
-        }) 
-
-        // Bouton d'edit de todo -> Avec prompt on affiche une popup dans laquelle on rentre la todo modifiée
-        // On récupère le h5 en passant ^pâr les Nodes enfants de notre todo div
-        editBtn.addEventListener("click", () => {
-            let newTodo = prompt("Modifiez votre todo : ")
-
-            if (newTodo != "") {
-                todoDiv.childNodes[0].childNodes[1].textContent = newTodo
-            }
+// Ici on attend que la page se charge avant d'afficher les todos depuis le LS 
+// a l'aide d'une fonction d'affichage displaytodo
+window.addEventListener("DOMContentLoaded", () => {
+    if (todosArray.length) {
+        todosArray.forEach(todo => {
+            displayTodo(todo.content, todo.check)
         })
-
-        // Bouton de suoppression -> quand on cliquie dessus on veut supprimer la todo de notre liste de todos
-        closeBtn.addEventListener("click", () => {
-            // Avec remove() on supprime la todo désirée
-            todoDiv.remove()
-        })
-
-        // Je regroupe mes boutons dans une div parent 
-        let divBtns = document.createElement("div")
-        // J'ajoute des classes tailwind pour styliser mon ensemble de boutons 
-        divBtns.classList.add("flex", "absolute", "top-0", "right-0")
-        // Je regroupe mes boutons dans une div commune pour des soucis de style
-        divBtns.append(checkBtn, editBtn, closeBtn)
-        
-        // J'ajoute ma div de boutons à ma div de todo
-        todoDiv.append(divBtns)
-
-        // Je viens "append" cad injecter ma todo fraichement créee dans ma liste de todos 
-        todosList.append(todoDiv)
     }
 })
 
-// Améliorer la todo : 
+// On gère la soumission d'une todo en appellant displayTodo 
+todoSubmit.addEventListener("click", () => {
+    if (todoInput.value != "") {
+        displayTodo(todoInput.value, false)
+    }    
+})
 
-// 1 - Faire en sorte que chaque todo affichée ait une vraie tete de todo (cf tailwind)
-// 2 - Il faut qu'elle ait un bouton de modification, de validation (pour checker) et de suppressin
-// Que ces boutons soient fonctionnels 
-// 3 - Le bouton de check doit afficher notre todo avec une faible opacité et le contenu texte barré 
-// BONUS : Une animation pour chaque todo ajoutée (type pop up ou autre voir animate css)
+// FONCTION D'AFFICHAGE DE TODO ET D'ENREGISTREMENT EN LS
+function displayTodo(content, check) {
+    // Je créee un élément HTML avec pour balise div
+    let todoDiv = document.createElement("div")
+    todoDiv.classList.add("relative")
+    // Il faudra ajouter de bons styles pour la todo 
+    todoDiv.innerHTML = `<a href="#" class="mt-2 bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs hover:bg-neutral-secondary-medium">
+            <h5 class="todoContent mb-3 text-xl font-semibold tracking-tight text-heading leading-8">` + content + `</h5>
+    </a>`
+
+    // Création des boutons (ou plutot mes inputs et img qui serviront de boutons) pour chaque div
+    let checkBtn = document.createElement("div")
+    let editBtn = document.createElement("a")
+    let closeBtn = document.createElement("a")
+
+    // Je donne du contenu à mes boutons 
+    checkBtn.innerHTML = `<input class="w-4" type="checkbox" />`
+    editBtn.innerHTML = `<img class="w-6 " src="./assets/icons/edit.svg" />`
+    closeBtn.innerHTML = `<img class="w-6 " src="./assets/icons/close-cross.svg" >`
+
+    // Logique du bouton check -> on écoute le click et on change les classes pour ajouter la ligne 
+    // et modifier l'opacité 
+    checkBtn.addEventListener("click", () => {
+        todoDiv.classList.toggle("line-through")
+        todoDiv.classList.toggle("opacity-50")
+    }) 
+
+    // Bouton d'edit de todo -> Avec prompt on affiche une popup dans laquelle on rentre la todo modifiée
+    // On récupère le h5 en passant ^pâr les Nodes enfants de notre todo div
+    editBtn.addEventListener("click", () => {
+        let newTodo = prompt("Modifiez votre todo : ")
+
+        if (newTodo != "") {
+            todoDiv.childNodes[0].childNodes[1].textContent = newTodo
+        }
+    })
+
+    // Bouton de suoppression -> quand on cliquie dessus on veut supprimer la todo de notre liste de todos
+    closeBtn.addEventListener("click", () => {
+        // Avec remove() on supprime la todo désirée
+        todoDiv.remove()
+    })
+
+    // Je regroupe mes boutons dans une div parent 
+    let divBtns = document.createElement("div")
+    // J'ajoute des classes tailwind pour styliser mon ensemble de boutons 
+    divBtns.classList.add("flex", "absolute", "top-0", "right-0")
+    // Je regroupe mes boutons dans une div commune pour des soucis de style
+    divBtns.append(checkBtn, editBtn, closeBtn)
+    
+    // J'ajoute ma div de boutons à ma div de todo
+    todoDiv.append(divBtns)
+
+    // Je viens "append" cad injecter ma todo fraichement créee dans ma liste de todos 
+    todosList.append(todoDiv)
+
+    let todoObject = {
+        content: todoDiv.childNodes[0].childNodes[1].textContent,
+        check: checkBtn.checked || check
+    }
+
+    // On ajoute à notre tableau de todos l'objet contenant les infos de notrre todo actuelle
+    todosArray.push(todoObject)
+
+    // Enfin onenregistre en Local Storage la version mise à jour du tableau
+    localStorage.setItem("todos", JSON.stringify(todosArray)) 
+}
+
+
+// J'ajoute au LS une paire de clé (nom) et valeur (jeanjean)
+localStorage.setItem("nom", "jeanjean")
+localStorage.getItem("nom") // Je récupère la valeur associée à nom depuis le LS
+
+
+// FAIRE FONCTIONNER LA TODO AVEC LE Local Storage
   
